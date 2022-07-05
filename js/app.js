3e01ee37a53e0642f13591137c188ba659e94448
MarcelaMs21 <menesesdiana237@gmail.com>
2022-07-04 20:40:28 -0500

    Carolina Spa

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.Swiper = new Swiper({
+     el: '.slider__contenedor',
+     slideClass: 'slider__slide',
+     createElements: true,
+
+     autoplay: {
+        delay: 5000
+     },
+     loop: true,
+     pagination: true,
+     navigation: true,
+  
+     
+});
